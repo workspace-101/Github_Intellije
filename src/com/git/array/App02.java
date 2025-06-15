9c88d4ccd12c91edecce0af51c570d384fcdc202
Pankaj <gpankaj0502@gmail.com>
2025-06-15 22:07:37 +0530

    sort the 0 and 1 using simple for loop

---
+++ src/com/git/array/App02.java
@@ -0,0 +1,43 @@
+package com.git.array;
+
+import java.util.Scanner;
+
+public class App02 {
+    public static void main(String[] args) {
+        int arr[]=creating_Arr();
+        sort_zero_and_one(arr);
+    }
+
+    private static void sort_zero_and_one(int arr[]) {  // arr[]={1,0,1,0,1,0,1,0}
+        int cnt=0;
+
+        int len=arr.length;
+        for(int i=0;i<len;i++){
+            if(arr[i]==0){
+                cnt++;
+            }
+        }
+        for(int i=0;i<cnt;i++){
+            arr[i]=0;
+        }
+        for(int i=cnt;i<len;i++){
+            arr[i]=1;
+        }
+        for(int i=0;i<len;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    private static int[] creating_Arr() {
+        Scanner cs=new  Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size=cs.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=cs.nextInt();
+        }
+        return arr;
+    }
+}
